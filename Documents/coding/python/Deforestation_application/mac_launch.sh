0d708f212f119e3a17bde01e7a241cdc5792b799
Joshua <72976308+joshuadarecullen@users.noreply.github.com>
2022-01-19 17:33:00 +0000

    Delete Documents/coding/python/Deforestation_application directory

--- Documents/coding/python/Deforestation_application/mac_launch.sh
+++
@@ -1,7 +0,0 @@
-export PYTHONPATH="$PYTHONPATH:/Applications/QGIS.app/Contents/Resources/python"
-export DYLD_FRAMEWORK_PATH="/Applications/QGIS.app/Contents/Frameworks"
-export PYTHONPATH="$PYTHONPATH:/Library/Frameworks/GDAL.framework/Versions/2.1/Python/2.7/site-packages"
-export QGIS_PREFIX="/Applications/QGIS.app/Contents/Resources"
-
-python deforestMain.py
-
